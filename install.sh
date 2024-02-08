4e6c1aa104b25ffa4042ea57c2cb202123a8823d
Kojo Jeffery <jefferyosei101@gmail.com>
2024-02-08 12:18:58 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -217,13 +217,11 @@ install_neovim()
   printf "Installing Neovim Latests...\n"
 
   sudo apt-get install software-properties-common
-  sudo apt-get install python-software-properties
+  sudo apt-get install python-dev python-pip python3-dev python3-pip
   
   sudo add-apt-repository ppa:neovim-ppa/stable
   sudo apt-get update
   sudo apt-get install neovim
-
-  sudo apt-get install python-dev python-pip python3-dev python3-pip
 
   nvim --version
 
